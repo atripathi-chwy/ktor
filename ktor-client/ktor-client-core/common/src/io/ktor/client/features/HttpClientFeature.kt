21f97d75baac13a3f46f0fa15ddb09290b5adb22
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-09-18 14:57:33 +0300

    KTOR-973 Allow HttpClient freeze

--- ktor-client/ktor-client-core/common/src/io/ktor/client/features/HttpClientFeature.kt
+++ ktor-client/ktor-client-core/common/src/io/ktor/client/features/HttpClientFeature.kt
@@ -43,6 +43,8 @@ public fun <B : Any, F : Any> HttpClient.feature(feature: HttpClientFeature<B, F
  * @throws [IllegalStateException] if [feature] is not installed.
  */
 public operator fun <B : Any, F: Any> HttpClient.get(feature: HttpClientFeature<B, F>): F {
+    feature(feature)?.let {  return it }
+
     val message = "Feature $feature is not installed. Consider using `install(${feature.key})` in client config first."
-    return feature(feature) ?: error(message)
+    error(message)
 }
